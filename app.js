2b99fb2e32c12d1ec54245fa6d6dbf6aa79aa625
isoyigido <ismailyigitozmen@gmail.com>
2025-02-05 00:45:51 +0300

    Bugfix
    Fixed selected language display

--- app.js
+++ app.js
@@ -10,16 +10,19 @@ function setLanguage(languageCode) {
 
             document.getElementById('selected-lang').textContent = data.language || 'Null';
             document.getElementById('selected-lang').style.setProperty('--flag-image', 'url(res/flags/' + languageCode + '.png)' || '#000');
-            document.getElementById('navbar-history').textContent = data.navbar_history || 'Null';
-            document.getElementById('navbar-modelling').textContent = data.navbar_modelling || 'Null';
-            document.getElementById('navbar-quiz').textContent = data.navbar_quiz || 'Null';
-
-            if(currentPage === '/index.html' || currentPage === '/') {
-                document.querySelector('.introduction .text').innerHTML = data.introduction_text || 'Null';
-                document.querySelector('.text-container.history .text').innerHTML = data.history_text || 'Null';
-                document.querySelector('.text-container.modelling .text').innerHTML = data.modelling_text || 'Null';
-                document.querySelector('.text-container.quiz .text').innerHTML = data.quiz_text || 'Null';
-            }
+            document.getElementById('navbar-committees').textContent = data.navbar_committees || 'Null';
+            document.getElementById('navbar-team').textContent = data.navbar_team || 'Null';
+            document.getElementById('navbar-applications').textContent = data.navbar_applications || 'Null';
+            document.getElementById('navbar-faq').textContent = data.navbar_faq || 'Null';
+            document.getElementById('navbar-forms').textContent = data.navbar_forms || 'Null';
+            
+//            if(currentPage === '/index.html' || currentPage === '/') {
+//                document.querySelector('.introduction .text').innerHTML = data.introduction_text || 'Null';
+//                document.querySelector('.text-container.history .text').innerHTML = data.history_text || 'Null';
+//                document.querySelector('.text-container.modelling .text').innerHTML = data.modelling_text || 'Null';
+//                document.querySelector('.text-container.quiz .text').innerHTML = data.quiz_text || 'Null';
+//            }
+            
         })
         .catch(error => {
             console.error('Error loading language file:', error);
