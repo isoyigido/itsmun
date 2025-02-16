7430ee6116ad956826bfdb5186e7d98a849691fb
isoyigido <ismailyigitozmen@gmail.com>
2025-02-16 14:49:24 +0300

    Bugfix

--- app.js
+++ app.js
@@ -24,23 +24,26 @@ function setLanguage(languageCode) {
         .then(data => {
             const currentPage = window.location.pathname;
 
-            document.getElementById('selected-lang').textContent = data.language || 'Null';
-            document.getElementById('selected-lang').style.setProperty('--flag-image', 'url(res/flags/' + languageCode + '.png)' || '#000');
-            document.getElementById('navbar-committees').textContent = data.navbar_committees || 'Null';
-            document.getElementById('navbar-team').textContent = data.navbar_team || 'Null';
-            document.getElementById('navbar-applications').textContent = data.navbar_applications || 'Null';
-            document.getElementById('navbar-faq').textContent = data.navbar_faq || 'Null';
-            document.getElementById('navbar-forms').textContent = data.navbar_forms || 'Null';
-            
-            if(currentPage === '/index.html' || currentPage === '/') {
-                document.getElementById('contact-text').textContent = data.contact_text || 'Null';
+            document.querySelectorAll('#selected-lang').forEach(el => {
+                el.textContent = data.language || 'Null';
+                el.style.setProperty('--flag-image', `url(res/flags/${languageCode}.png)`);
+            });
+
+            document.querySelectorAll('#navbar-committees').forEach(el => el.textContent = data.navbar_committees || 'Null');
+            document.querySelectorAll('#navbar-team').forEach(el => el.textContent = data.navbar_team || 'Null');
+            document.querySelectorAll('#navbar-applications').forEach(el => el.textContent = data.navbar_applications || 'Null');
+            document.querySelectorAll('#navbar-faq').forEach(el => el.textContent = data.navbar_faq || 'Null');
+            document.querySelectorAll('#navbar-forms').forEach(el => el.textContent = data.navbar_forms || 'Null');
+
+            if (currentPage === '/index.html' || currentPage === '/') {
+                document.querySelectorAll('#contact-text').forEach(el => el.textContent = data.contact_text || 'Null');
             }
-            
         })
         .catch(error => {
             console.error('Error loading language file:', error);
         });
 }
+
 
 window.onload = function() {
     const savedLanguage = localStorage.getItem('language') || 'en';
